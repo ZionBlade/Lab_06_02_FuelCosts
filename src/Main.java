e0fac7c842239ce21f1abe10ec05d1f9f667390c
ZionBlade <wernerct@mail.uc.edu>
2022-09-29 13:32:21 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        String trash = "";
+        double gallons = 0;
+        double economy = 0;
+        double price = 0;
+        double distance = 0;
+        double cost = 0;
+
+        System.out.print("Enter the number of gallons of gas in your tank: ");
+        if(in.hasNextDouble())
+        {
+            gallons = in.nextDouble();
+            in.nextLine();
+
+            System.out.print("Please enter the fuel efficiency of your car: ");
+            if(in.hasNextDouble())
+            {
+                economy = in.nextDouble();
+                in.nextLine();
+
+                System.out.print("Please enter the price per gallon: ");
+                if(in.hasNextDouble())
+                {
+                    cost = (price / economy) * 100;
+                    System.out.println("The cost to travel 100 miles would be: " + cost);
+
+                    distance = gallons * economy;
+                    System.out.println("On " + gallons + "gallons of gas, at " + economy + "miles per gallon you can travel " + distance);
+
+                }
+            }
+        }
+    }
+}
